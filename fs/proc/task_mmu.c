b3060f025ed533ea28bd7175cd240fead05333e0
fangqinwu <fangqinwu@xiaomi.com>
2021-02-11 18:19:38 +0530

    mm: some optimization for reclaim
    BSPSYS-10620
    
    This change includes:
     - fix reclaim count calculation issue
     - only reclaim un-shared pages
     - avoid burning CPU when nr_to_reclaim pages
     - export reclaim_pte_range() API for rtmm
    
    Change-Id: I04e77a2811f5ac4cae6d261c3eaaf88e26eee2ce
    Signed-off-by: fangqinwu <fangqinwu@xiaomi.com>
    
    darkhz:
    	- Remove rtmm_reclaim related changes
    	- Correct commit message

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1658,6 +1658,9 @@ cont:
 		if (!page)
 			continue;
 
+		if (page_mapcount(page) != 1)
+			continue;
+
 		if (isolate_lru_page(page))
 			continue;
 
@@ -1751,6 +1754,7 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	unsigned long start = 0;
 	unsigned long end = 0;
 	struct reclaim_param rp;
+	int ret;
 
 	memset(buffer, 0, sizeof(buffer));
 	if (count > sizeof(buffer) - 1)
@@ -1826,9 +1830,11 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			walk_page_range(max(vma->vm_start, start),
+			ret = walk_page_range(max(vma->vm_start, start),
 					min(vma->vm_end, end),
 					&reclaim_walk);
+			if (ret)
+				break;
 			vma = vma->vm_next;
 		}
 	} else {
@@ -1843,8 +1849,10 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			walk_page_range(vma->vm_start, vma->vm_end,
+			ret = walk_page_range(vma->vm_start, vma->vm_end,
 				&reclaim_walk);
+			if (ret)
+				break;
 		}
 	}
 
